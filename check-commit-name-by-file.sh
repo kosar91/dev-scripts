c1a8d2793b30c0378f6b1b82b29df2ef08ad81f7
Roman Kosartsov <roman.kosartsov@fingular.com>
2023-06-02 13:55:30 +0300

    feat: Added scripts and pre-commit hooks config

--- check-commit-name-by-file.sh
+++ check-commit-name-by-file.sh
@@ -2,4 +2,17 @@
 
 MESSAGE=$(cat "$1")
 
-bash check-commit-name.sh "$MESSAGE"
+COMMITFORMAT="^(feat|fix|docs|style|refactor|test|chore|perf|other)(\((.*)\))?: (.*)$"
+
+if ! [[ "$MESSAGE" =~ $COMMITFORMAT ]]; then
+  echo "Your commit message \"$MESSAGE\" has invalid format."
+  echo ""
+  echo "Please use the following format:"
+  echo "feat: feature example comment"
+  echo "feat: [DEV-12345] feature example comment"
+  echo "fix(ui): [DEV-54321] bugfix example comment"
+  exit 1
+else
+  echo "Your commit \"$MESSAGE\" was accepted"
+  exit 0
+fi
